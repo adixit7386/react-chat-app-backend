a3e69a0d7abc0cfc4ae40cb1708ef1bb09878b82
Abhay Dixit <adixit7386@gmail.com>
2023-01-21 23:34:00 +0530

    corrected existing user logic

--- controllers/AddToGroupControllers.js
+++ controllers/AddToGroupControllers.js
@@ -4,13 +4,12 @@ const Message = require("../models/Message");
 const customErrorHandler = require("../services/customErrorHandler");
 const AddToGroupControllers = async (req, res, next) => {
   const { userId, ChatId } = req.body;
-
   try {
     const chat = await Chat.find({
       $and: [{ _id: { $eq: ChatId } }, { users: { $in: userId } }],
     });
 
-    if (chat) {
+    if (chat.length !== 0) {
       return next(customErrorHandler.alreadyExist("This user already exists"));
     }
     const updatedChat = await Chat.findByIdAndUpdate(
