4c747a2c3601938ff1a0afe333e592d23dc6efb3
Abhay Dixit <adixit7386@gmail.com>
2023-01-31 02:39:29 +0530

    added updated origin

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://livechat-adixit7386.netlify.app/",
   },
 });
 io.on("connection", (socket) => {
